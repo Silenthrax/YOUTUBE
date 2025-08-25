fc492a9e6b628e6905264a9deb78e0e7be9bc361
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-08-25 20:41:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -112,14 +112,14 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 # 🖼️ Image URLs (Can be customized)
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/eehxb4.jpg")
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/pzp1ty.jpg")
+PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/pzp1ty.jpg")
+PLAYLIST_IMG_URL = "https://files.catbox.moe/pzp1ty.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/pzp1ty.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/pzp1ty.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/pzp1ty.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/pzp1ty.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/pzp1ty.jpg"
 YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
