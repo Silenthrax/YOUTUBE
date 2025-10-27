dabdc2e5939ca3b2984ca67ae1e2f7a8d23e81b0
Silenthrax <210396117+Silenthrax@users.noreply.github.com>
2025-10-27 21:25:24 +0530

    SILENTHRAX

--- ShrutiMusic/__init__.py
+++ ShrutiMusic/__init__.py
@@ -1,26 +1,4 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
-from ShrutiMusic.core.bot import Aviax
+from ShrutiMusic.core.bot import Nand
 from ShrutiMusic.core.dir import dirr
 from ShrutiMusic.core.git import git
 from ShrutiMusic.core.userbot import Userbot
@@ -33,7 +11,7 @@ git()
 dbb()
 heroku()
 
-app = Aviax()
+app = Nand()
 userbot = Userbot()
 
 
@@ -46,15 +24,3 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
